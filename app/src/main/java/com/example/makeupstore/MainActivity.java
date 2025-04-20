7fc485ca103102102786535bffb18734c17f9cf1
sarahrasheed <41295727+sarahrasheed@users.noreply.github.com>
2025-04-21 00:08:05 +0300

    version1

--- app/src/main/java/com/example/makeupstore/MainActivity.java
+++ app/src/main/java/com/example/makeupstore/MainActivity.java
@@ -36,9 +36,17 @@ public class MainActivity extends AppCompatActivity {
             return gson.fromJson(json, type);
         } else {
             List<Product> products = new ArrayList<>();
-            products.add(new Product(1, "Red Lipstick", 35.00, "lipstick_red", 10));
-            products.add(new Product(2, "Foundation Cream", 50.00, "foundation_nude", 5));
-            products.add(new Product(3, "Mascara Black", 25.00, "mascara_black", 8));
+            products.add(new Product(1, "Red Lipstick", 29.99, "redlipstick", 10));
+            products.add(new Product(2, "Liquid Foundation", 42.50, "foundation", 8));
+            products.add(new Product(3, "Mascara", 24.00, "mascara", 12));
+            products.add(new Product(4, "Blush Palette", 35.75, "blush", 6));
+            products.add(new Product(5, "Eyebrow Pencil", 14.90, "eyebrow", 15));
+            products.add(new Product(6, "Eyeshadow Pallete", 39.99, "eyeshadow", 7));
+            products.add(new Product(7, "Setting Spray", 21.00, "settingspray", 9));
+            products.add(new Product(8, "Beauty Blender", 10.00, "beautyblender", 20));
+            products.add(new Product(9, "Highlighter", 19.95, "highlighter", 10));
+            products.add(new Product(10, "Lip Gloss", 17.99, "lipgloss", 13));
+
 
             SharedPreferences.Editor editor = prefs.edit();
             editor.putString("products", gson.toJson(products));
